30fec90fee212bc16f3c8e625d399ebc2c42fb91
Jorge de Souza <Jorgejrdj1@gmail.com>
2023-03-30 19:28:53 -0300

    feat: add form

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -18,17 +18,19 @@ export function Home() {
                 Sexta, 31 de março de 2023.
             </Text>
 
-            <TextInput
-                style={styles.input}
-                placeholder="Nome do participante"
-                placeholderTextColor="#6B6B6B"
-            />
+            <View style={styles.form}>
+                <TextInput
+                    style={styles.input}
+                    placeholder="Nome do participante"
+                    placeholderTextColor="#6B6B6B"
+                />
 
-            <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
-                <Text style={styles.buttonText}>
-                    +
-                </Text>
-            </TouchableOpacity>
+                <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+                    <Text style={styles.buttonText}>
+                        +
+                    </Text>
+                </TouchableOpacity>
+            </View>
         </View>
     )
 }
